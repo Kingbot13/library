fb0c1311a9eeb4a61ab22899ec42cb9d7cc10488
Dylan King <dwkblue51@yahoo.com>
2021-07-02 12:48:00 -0500

    add function to update display

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const display = document.querySelector(".display");
 const submitBtn = document.querySelector("#submit");
+const log = console.log;
 
 let myLibrary = [];
+
+// manually create book and push to array for testing
+
+const book1 = new Book("Bob", "Bob The Builder", "200", true);
+
+myLibrary.push(book1);
 
 function Book(author, title, pages, read){
     this.author = author;
@@ -24,3 +31,35 @@ function addBook(){
     myLibrary.push(book);
 };
 
+log(myLibrary);
+
+// loop through array and update display
+
+function updateDisplay(){
+    myLibrary.forEach((item) => {
+        // create div to hold components
+        let card = document.createElement("div");
+        card.classList.add("card");
+        // create text to append to card variable
+        let author = document.createElement("p");
+        let title = document.createElement("p");
+        let pages = document.createElement("p");
+        let isRead = document.createElement("p");
+
+        author.textContent = item.author;
+        title.textContent = item.title;
+        pages.textContent = item.pages;
+        isRead.textContent = item.isRead;
+        
+        let textArray = [author, title, pages, isRead];
+
+        // loop through text content to append to card
+        textArray.forEach((text) => {
+            card.appendChild(text);
+        });
+
+        display.appendChild(card);
+    });
+};
+
+updateDisplay();
