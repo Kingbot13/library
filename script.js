9a96692d3491c3805e7031ef5faaa858c2acb0fa
Dylan King <dwkblue51@yahoo.com>
2021-07-01 20:53:51 -0500

    create book constructor function and function to add book to array

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// script
+const display = document.querySelector(".display");
+const submitBtn = document.querySelector("#submit");
+
+let myLibrary = [];
+
+function Book(author, title, pages, read){
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+};
+
+function addBook(){
+    // create new book
+    let author = document.querySelector("#author").value;
+    let title = document.querySelector("#title").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read");
+
+    let book = new Book(author, title, pages, read);
+
+    // push book to array
+
+    myLibrary.push(book);
+};
+
